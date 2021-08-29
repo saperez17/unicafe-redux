7b323fe091a3f9ece1f39b886f6421a85e326760
Santiago Alvarado <34078372+saperez17@users.noreply.github.com>
2021-08-29 14:12:17 -0500

    unicafe revisited exercises 6.1-6.2 implementation of the 6.1-6.2 exercises

--- src/index.js
+++ src/index.js
@@ -1,33 +1,55 @@
-import React from 'react';
-import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
-import reducer from './reducer'
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import reducer from "./reducer";
 
-const store = createStore(reducer)
+const store = createStore(reducer);
 
 const App = () => {
   const good = () => {
     store.dispatch({
-      type: 'GOOD'
-    })
-  }
+      type: "GOOD"
+    });
+  };
+  const dispatchers = {
+    good: () => {
+      store.dispatch({
+        type: "GOOD"
+      });
+    },
+    bad: () => {
+      store.dispatch({
+        type: "BAD"
+      });
+    },
+    neutral: () => {
+      store.dispatch({
+        type: "NEUTRAL"
+      });
+    },
+    zero: () => {
+      store.dispatch({
+        type: "ZERO"
+      });
+    }
+  };
 
   return (
     <div>
-      <button onClick={good}>good</button> 
-      <button>neutral</button> 
-      <button>bad</button>
-      <button>reset stats</button>
+      <button onClick={dispatchers.good}>good</button>
+      <button onClick={dispatchers.neutral}>neutral</button>
+      <button onClick={dispatchers.bad}>bad</button>
+      <button onClick={dispatchers.zero}>reset stats</button>
       <div>good {store.getState().good}</div>
-      <div>neutral</div>
-      <div>bad</div>
+      <div>neutral {store.getState().neutral}</div>
+      <div>bad {store.getState().bad}</div>
     </div>
-  )
-}
+  );
+};
 
 const renderApp = () => {
-  ReactDOM.render(<App />, document.getElementById('root'))
-}
+  ReactDOM.render(<App />, document.getElementById("root"));
+};
 
-renderApp()
-store.subscribe(renderApp)
+renderApp();
+store.subscribe(renderApp);
